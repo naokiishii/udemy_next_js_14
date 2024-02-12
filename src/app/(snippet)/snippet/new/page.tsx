7e34fd8995dd746a1a9178e03d43e3ff1ac6368e
Naoki <ishii.naoki@ymail.ne.jp>
2024-02-13 08:53:46 +0900

    section 1 + 2

---
+++ src/app/(snippet)/snippet/new/page.tsx
@@ -0,0 +1,32 @@
+export default function SnippetCreatePage() {
+  return (
+    <form>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          ></input>
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="codd"
+          ></textarea>
+        </div>
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
