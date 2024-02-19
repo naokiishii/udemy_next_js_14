12091493fd05dd9592b2a9bf5ad365a689199f98
Naoki <ishii.naoki@ymail.ne.jp>
2024-02-19 09:05:38 +0900

    section 3 - missing files

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -2,14 +2,14 @@
 
 import type { Snippet } from "@prisma/client";
 import { Editor } from "@monaco-editor/react";
-import { useState } from 'react'
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
-  const [code, setCode] = useState(snippet.code)
+  const [code, setCode] = useState(snippet.code);
   const handleEditorChange = (value: string = "") => {
     setCode(value);
   };
