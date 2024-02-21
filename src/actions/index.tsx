4d42de39bdfb2cfd5f4397432787e6312708d99a
Naoki <ishii.naoki@ymail.ne.jp>
2024-02-22 08:53:11 +0900

    Section 5

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -15,5 +15,55 @@ export async function deleteSnippet(id: number) {
   await db.snippet.delete({
     where: { id },
   });
-  redirect('/snippet');
+  redirect("/snippet");
+}
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    /*
+  return {
+    message: "Title must be longer.",
+  };
+  */
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer.",
+      };
+    }
+
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer.",
+      };
+    }
+
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title: title,
+        code: code,
+      },
+    });
+    console.log(snippet);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong.",
+      };
+    }
+  }
+
+  // Redirect the user back to the root route
+  redirect("/snippet");
 }
