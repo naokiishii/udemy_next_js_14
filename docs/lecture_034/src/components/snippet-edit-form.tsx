4fa6068f14ec6788f8c47463a6b1386a5e01e25b
Naoki <ishii.naoki@ymail.ne.jp>
2024-03-09 07:28:58 +0900

    Documents for Section3

---
+++ docs/lecture_034/src/components/snippet-edit-form.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import type { Snippet } from '@prisma/client';
+import Editor from '@monaco-editor/react';
+
+interface SnippetEditFormProps {
+  snippet: Snippet;
+}
+
+export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+      />
+    </div>
+  );
+}
