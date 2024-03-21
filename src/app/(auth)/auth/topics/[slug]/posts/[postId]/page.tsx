d87b0ebf37bb2837ab3330439229a146c45fb86f
Naoki <ishii.naoki@ymail.ne.jp>
2024-03-22 08:03:21 +0900

    Section 8

--- src/app/(auth)/auth/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/(auth)/auth/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,28 @@
-export default function PostShowPage() {
-  return <div>Post Show Page</div>
+import Link from "next/link";
+import PostShow from "@/app/(auth)/components/posts/post-show";
+import CommentList from "@/app/(auth)/components/comments/comment-list";
+import CommentCreateForm from "@/app/(auth)/components/comments/comment-create-form";
+import paths from "@/app/(auth)/path";
+import { fetchCommentsByPostId } from "@/db/queries/comments";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShowPath(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
+    </div>
+  );
 }
--- src/app/(auth)/components/topics/topic-create-form.tsx
+++ src/app/(auth)/components/topics/topic-create-form.tsx
@@ -9,6 +9,7 @@ import {
   PopoverContent,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
+import FormButton from "../common/form-button";
 
 export default function TopicCreateForm() {
   const [formState, action] = useFormState(actions.createTopic, {
@@ -44,7 +45,7 @@ export default function TopicCreateForm() {
                 {formState.errors._form.join(", ")}
               </div>
             ) : null}
-            <Button type="submit">Submit</Button>
+            <FormButton>Save</FormButton>
           </div>
         </form>
       </PopoverContent>
