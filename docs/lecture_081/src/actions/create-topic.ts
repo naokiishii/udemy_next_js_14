252824cf687db4f65a75d351920c802621c3cb12
Naoki <ishii.naoki@ymail.ne.jp>
2024-03-22 08:13:18 +0900

    Sectiono 7 - docs

---
+++ docs/lecture_081/src/actions/create-topic.ts
@@ -0,0 +1,53 @@
+'use server';
+
+import { z } from 'zod';
+import { auth } from '@/auth';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to do this.'],
+      },
+    };
+  }
+
+  return {
+    errors: {},
+  };
+
+  // TODO: revalidate the homepage
+}
